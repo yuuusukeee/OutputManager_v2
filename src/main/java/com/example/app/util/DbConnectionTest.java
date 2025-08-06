20dfd661bd52e79e5ca70dba0652d57d9fb803e3
yuuusukeee <daddykwkm0130@gmail.com>
2025-08-06 15:24:58 +0900

    DBテスト成功

---
+++ src/main/java/com/example/app/util/DbConnectionTest.java
@@ -0,0 +1,20 @@
+package com.example.app.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbConnectionTest implements CommandLineRunner{
+
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public void run(String...args) {
+		var users=jdbcTemplate.queryForList("SELECT*FROM users");
+		System.out.println("【DB疎通OK】:"+users);
+	}
+}
