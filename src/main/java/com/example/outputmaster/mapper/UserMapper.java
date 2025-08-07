572da223fd21550bf5c87e96db78fed409200fc5
yuuusukeee <daddykwkm0130@gmail.com>
2025-08-07 22:37:34 +0900

    UserMapper実装

---
+++ src/main/java/com/example/outputmaster/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.outputmaster.mapper;
+
+import com.example.outputmaster.domain.User;
+
+public interface UserMapper {
+	 /** ユーザー名からユーザーを1件取得 */
+	User selectByUsername(String username);
+    /** 新規ユーザー登録 */
+	void insert(User user);
+    /** IDからユーザー取得 */
+	User selectById(Integer id);
+}
