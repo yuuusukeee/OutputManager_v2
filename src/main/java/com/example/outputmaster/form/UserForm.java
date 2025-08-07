fc6912dc6ce876df5be7f6f2e68cc70d1d9b97cb
yuuusukeee <daddykwkm0130@gmail.com>
2025-08-07 15:49:42 +0900

    UserForm.Java実装

---
+++ src/main/java/com/example/outputmaster/form/UserForm.java
@@ -0,0 +1,33 @@
+package com.example.outputmaster.form;
+
+import jakarta.validation.constraints.NotBlank;
+
+import com.example.outputmaster.domain.User;
+
+import lombok.Data;
+
+@Data
+public class UserForm {
+    /** ユーザー名（必須） */
+	@NotBlank
+	private String username;
+	
+    /** メールアドレス（形式・必須） */
+	@NotBlank
+	private String email;
+	
+    /** パスワード（必須） */
+	@NotBlank
+	private String password;
+	
+    /** toEntityメソッド：Userエンティティへ変換 */
+	
+	public User toEntity() {
+		User user=new User();
+		user.setUsername(this.username);
+		user.setEmail(this.email);
+		user.setPassword(this.password);
+		return user;
+	}
+
+}
